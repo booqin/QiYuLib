e2e5a603d09e34d825c59ed4e864dff32dfc4986
vito <developervito@163.com>
2018-06-05 17:40:07 +0800

    1

--- app/src/main/java/com/xinguang/qiyudemo/MyApplication.java
+++ app/src/main/java/com/xinguang/qiyudemo/MyApplication.java
@@ -14,7 +14,7 @@ public class MyApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-        UnicornSDK.init(this, "b103e36c541d05aa60cdc9413fbb2e45").apply();
+        UnicornSDK.init(this, "your key").apply();
     }
 
 }
